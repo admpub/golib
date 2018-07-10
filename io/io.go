123e7d49812d1dda2f5f4daba8f2c4765ef748be
Wenhui Shen <swhbox@foxmail.com>
2018-07-10 22:11:35 +0800

    change import path

--- io/io.go
+++ io/io.go
@@ -18,8 +18,8 @@ import (
 	"io"
 	"sync"
 
-	"github.com/fatedier/golib/crypto"
-	"github.com/fatedier/golib/pool"
+	"github.com/admpub/golib/crypto"
+	"github.com/admpub/golib/pool"
 )
 
 // Join two io.ReadWriteCloser and do some operations.
